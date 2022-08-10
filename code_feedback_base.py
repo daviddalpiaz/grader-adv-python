58c49b849cffe7c453e418501f84c3ac3543af87
James Balamuta <balamut2@illinois.edu>
2022-08-10 10:58:05 -0700

    Rename/add the base version of code_feedback.py for testing purposes.

--- code_feedback_base.py
+++ code_feedback_base.py
@@ -2,7 +2,7 @@
 
 from __future__ import division, print_function
 
-__copyright__ = "Copyright (C) 2014 - Current: Andreas Kloeckner, 2022: James J Balamuta"
+__copyright__ = "Copyright (C) 2014 Andreas Kloeckner"
 
 __license__ = """
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -11,8 +11,10 @@ in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -57,6 +59,7 @@ class Feedback:
     def set_score(cls, score):
         """
         Feedback.set_score(percentage)
+
         Set the score for the test case, should be a floating point value between 0 and 1.
         """
         if score < 0:
@@ -78,6 +81,7 @@ class Feedback:
     def add_feedback(cls, text):
         """
         Feedback.add_feedback(text)
+
         Adds some text to the feedback output for the current test.
         """
         with open("/grade/run/" + cls.feedback_file + ".txt", 'a+',
@@ -90,6 +94,7 @@ class Feedback:
     def finish(cls, fb_text):
         """
         Feedback.finish(fb_text)
+
         Complete grading immediately, additionally outputting the message in fb_text.
         """
         cls.add_feedback(fb_text)
@@ -99,6 +104,7 @@ class Feedback:
     def not_allowed(*args, **kwargs):
         """
         library_function = Feedback.not_allowed
+
         Used to hook into disallowed functions, raises an exception if
         the student tries to call it.
         """
@@ -108,7 +114,9 @@ class Feedback:
     def check_numpy_array_sanity(cls, name, num_axes, data):
         """
         Feedback.check_numpy_array_sanity(name, num_axes, data)
+
         Perform a sanity check on a NumPy array, making sure that it is in fact defined and has the correct dimensionality.  If the checks fail then grading will automatically stop.
+
         - ``name``: Name of the array that is being checked.  This will be used to give feedback.
         - ``num_axes``: Number of axes that the array should have.
         - ``data``: NumPy array to check.
@@ -116,24 +124,25 @@ class Feedback:
 
         import numpy as np
         if data is None:
-            cls.finish(f"'{name}' is None or not defined")
+            cls.finish("'%s' is None or not defined" % name)
 
         if not isinstance(data, np.ndarray):
-            cls.finish(f"'{name}' is not a numpy array")
+            cls.finish("'%s' is not a numpy array" % name)
 
         if isinstance(data, np.matrix):
-            cls.finish(f"'{name}' is a numpy matrix. Do not use those. "
-                       "bit.ly/array-vs-matrix")
+            cls.finish("'%s' is a numpy matrix. Do not use those. "
+                       "bit.ly/array-vs-matrix" % name)
 
         if len(data.shape) != num_axes:
             cls.finish(
-                    f"'{name}' does not have the correct number of axes--"
-                    f"got: {len(data.shape)}, expected: {num_axes}")
+                    "'%s' does not have the correct number of axes--"
+                    "got: %d, expected: %d" % (
+                        name, len(data.shape), num_axes))
 
         if data.dtype.kind not in "fc":
             cls.finish(
-                    f"'{name}' does not consist of floating point numbers--"
-                    f"got: '{data.dtype}'")
+                    "'%s' does not consist of floating point numbers--"
+                    "got: '%s'" % (name, data.dtype))
 
     @classmethod
     def check_numpy_array_features(cls, name, ref, data,
@@ -141,7 +150,9 @@ class Feedback:
                                    report_failure=True):
         """
         Feedback.check_numpy_array_features(name, ref, data)
+
         Check that a student NumPy array has the same shape and datatype as a  reference solution NumPy array.
+
         - ``name``: Name of the array that is being checked.  This will be used to give feedback.
         - ``ref``: Reference NumPy array.
         - ``data``: Student NumPy array to be checked.  Do not mix this up with the previous array! This argument is subject to more strict type checking.
@@ -160,24 +171,26 @@ class Feedback:
                 return False
 
         if data is None:
-            return bad(f"'{name}' is None or not defined")
+            return bad("'%s' is None or not defined" % name)
 
         if not isinstance(data, np.ndarray):
-            return bad(f"'{name}' is not a numpy array")
+            return bad("'%s' is not a numpy array" % name)
 
         if isinstance(data, np.matrix):
-            return bad(f"'{name}' is a numpy matrix. Do not use those. "
-                       "bit.ly/array-vs-matrix")
+            return bad("'%s' is a numpy matrix. Do not use those. "
+                       "bit.ly/array-vs-matrix" % name)
 
         if ref.shape != data.shape:
             return bad(
-                    f"'{name}' does not have correct shape--"
-                    f"got: '{data.shape}', expected: '{ref.shape}'")
+                    "'%s' does not have correct shape--"
+                    "got: '%s', expected: '%s'" % (
+                        name, data.shape, ref.shape))
 
         if ref.dtype.kind != data.dtype.kind:
             return bad(
-                    f"'{name}' does not have correct data type--"
-                    f"got: '{data.dtype}', expected: '{ref.dtype}'")
+                    "'%s' does not have correct data type--"
+                    "got: '%s', expected: '%s'" % (
+                        name, data.dtype, ref.dtype))
 
         return True
 
@@ -188,8 +201,10 @@ class Feedback:
                                    report_failure=True):
         """
         Feedback.check_numpy_allclose(name, ref, data)
+
         Check that a student NumPy array has similar values to a reference NumPy array. Note that this checks value according to the numpy.allclose function, which goes  by the following check:
         ``absolute(a - b) <= (atol + rtol * absolute(b))``
+
         - ``name``: Name of the array that is being checked.  This will be used to give feedback.
         - ``ref``: Reference NumPy array.
         - ``data``: Student NumPy array to be checked.  Do not mix this up with the previous array! This argument is subject to more strict type checking.
@@ -210,10 +225,10 @@ class Feedback:
 
         if not good:
             if report_failure:
-                cls.add_feedback(f"'{name}' is inaccurate")
+                cls.add_feedback("'%s' is inaccurate" % name)
         else:
             if report_success:
-                cls.add_feedback(f"'{name}' looks good")
+                cls.add_feedback("'%s' looks good" % name)
 
         if accuracy_critical and not good:
             raise GradingComplete()
@@ -226,7 +241,9 @@ class Feedback:
                             report_failure=True):
         """
         Feedback.check_list(name, ref, data)
+
         Check that a student list has correct length with respect to a reference list.  Can also check for a homogeneous data type for the list.
+
         - ``name``: Name of the list that is being checked.  This will be used to give feedback.
         - ``ref``: Reference list.
         - ``data``: Student list to be checked.  Do not mix this up with the previous list! This argument is subject to more strict type checking.
@@ -244,18 +261,19 @@ class Feedback:
                 return False
 
         if data is None:
-            return bad(f"'{name}' is None or not defined")
+            return bad("'%s' is None or not defined" % name)
 
         if not isinstance(data, list):
-            return bad(f"'{name}' is not a list")
+            return bad("'%s' is not a list" % name)
 
         if len(ref) != len(data):
-            return bad(f"'{name}' has the wrong length--expected {len(ref)}, got {len(data)}")
+            return bad("'%s' has the wrong length--expected %d, got %d"
+                       % (name, len(ref), len(data)))
 
         if entry_type is not None:
             for i, entry in enumerate(data):
                 if not isinstance(entry, entry_type):
-                    return bad(f"'{name}[{i}]' has the wrong type")
+                    return bad("'%s[%d]' has the wrong type" % (name, i))
 
         return True
 
@@ -264,7 +282,9 @@ class Feedback:
                     report_failure=True, report_success=True):
         """
         Feedback.check_tuple(name, ref, data)
+
         Check that a student tuple has correct length with respect to a reference tuple, and same values.
+
         - ``name``: Name of the tuple that is being checked.  This will be used to give feedback.
         - ``ref``: Reference tuple.
         - ``data``: Student tuple to be checked.  Do not mix this up with the previous tuple! This argument is subject to more strict type checking.
@@ -283,29 +303,30 @@ class Feedback:
                 return False
 
         if data is None:
-            return bad(f"{name} is None or not defined")
+            return bad("{} is None or not defined".format(name))
 
         if not isinstance(data, tuple):
-            return bad(f"{name} is not a tuple")
+            return bad("{} is not a tuple".format(name))
 
         nref = len(ref)
         if len(data) != nref:
-            return bad(f"{name} should be of length {nref}")
+            return bad("{} should be of length {}".format(name, nref))
 
         good = True
         for i in range(nref):
             if type(data[i]) != type(ref[i]):  # noqa: E721
                 good = False
                 if report_failure:
-                    cls.add_feedback(f"{name}[{i}] should be of type {type(ref[i]).__name__}")
+                    cls.add_feedback("{}[{}] should be of type {}"
+                                     .format(name, i, type(ref[i]).__name__))
             elif data[i] != ref[i]:
                 good = False
 
         if not good:
-            return bad(f"'{name}' is inaccurate")
+            return bad("'%s' is inaccurate" % name)
         else:
             if report_success:
-                cls.add_feedback(f"'{name}' looks good")
+                cls.add_feedback("'%s' looks good" % name)
 
         return True
 
@@ -315,11 +336,15 @@ class Feedback:
                      report_failure=True):
         """
         Feedback.check_scalar(name, ref, data)
+
         Check that a student scalar has correct value with respect to a reference scalar. This will mark a value as correct if it passes any of the following checks:
+
         - ``abs(ref - data) < ref(ref) * rtol``
         - ``abs(ref - data) < atol``
+
         One of rtol or atol can be omitted (set to None) if that check is unwanted.
         Or both, but then nothing would be graded :)
+
         - ``name``: Name of the scalar that is being checked.  This will be used to give feedback.
         - ``ref``: Reference scalar.
         - ``data``: Student scalar to be checked.  Do not mix this up with the previous value! This argument is subject to more strict type checking.
@@ -342,7 +367,7 @@ class Feedback:
                 return False
 
         if data is None:
-            return bad(f"'{name}' is None or not defined")
+            return bad("'%s' is None or not defined" % name)
 
         if not isinstance(data, (complex, float, int, np.number)):
             try:
@@ -350,9 +375,9 @@ class Feedback:
                 # numbers do not follow the typical interface
                 # See https://github.com/inducer/relate/pull/284
                 if not data.is_number:
-                    return bad(f"'{name}' is not a number")
+                    return bad("'%s' is not a number" % name)
             except AttributeError:
-                return bad(f"'{name}' is not a number")
+                return bad("'%s' is not a number" % name)
 
         good = False
 
@@ -362,10 +387,10 @@ class Feedback:
             good = True
 
         if not good:
-            return bad(f"'{name}' is inaccurate")
+            return bad("'%s' is inaccurate" % name)
         else:
             if report_success:
-                cls.add_feedback(f"'{name}' looks good")
+                cls.add_feedback("'%s' looks good" % name)
 
         return True
 
@@ -373,7 +398,9 @@ class Feedback:
     def call_user(cls, f, *args, **kwargs):
         """
         Feedback.call_user(f)
+
         Attempts to call a student defined function, with any arbitrary arguments specified in ``*args`` and ``**kwargs``.  If the student code raises an exception, this will be caught and user feedback will be given.
+
         If the function call succeeds, the user return value will be returned from this function.
         """
 
@@ -385,14 +412,19 @@ class Feedback:
                     callable_name = f.__name__
                 except Exception as e_name:
                     callable_name = (
-                                f"<unable to retrieve name; encountered {type(e_name).__name__}: {str(e_name)}>"
-                              )
+                                "<unable to retrieve name; encountered %s: %s>"
+                                % (
+                                    type(e_name).__name__,
+                                    str(e_name)))
                 from traceback import format_exc
                 cls.add_feedback(
-                        f"The callable '{callable_name}' supplied in your code failed with "
+                        "The callable '%s' supplied in your code failed with "
                         "an exception while it was being called by the "
                         "grading code:"
-                        f"{''.join(format_exc())}")
+                        "%s"
+                        % (
+                            callable_name,
+                            "".join(format_exc())))
             else:
                 cls.add_feedback(
                         "Your code was supposed to supply a function or "
@@ -407,7 +439,9 @@ class Feedback:
                    report_success=True):
         """
         Feedback.check_plot(name, ref, plot, check_axes_scale)
+
         Checks that a student plot has the same lines as a reference plot solution. Can optionally check the axis scales to ensure they are the same as the reference.
+
         - ``name``: Name of plot scalar that is being checked.  This will be used to give feedback.
         - ``ref``: Reference plot.
         - ``data``: Student plot to be checked.  Do not mix this up with the previous value! This argument is subject to more strict type checking.
@@ -430,10 +464,10 @@ class Feedback:
                 return False
 
         if plot is None:
-            return bad(f"'{name}' is None or not defined")
+            return bad("'%s' is None or not defined" % name)
 
         if not isinstance(plot, matplotlib.axes.Axes):
-            return bad(f"'{name}' is not an object of matplotlib axes")
+            return bad("'%s' is not an object of matplotlib axes" % name)
 
         # check_axes_scale can be None, 'x', 'y', or 'xy'
         if check_axes_scale:
@@ -452,18 +486,18 @@ class Feedback:
                     scales_match = False
 
             if not scales_match:
-                return bad(f"'{name}' does not have the correct scale for its axes")
+                return bad("'%s' does not have the correct scale for its axes" % name)
 
         user_lines = plot.get_lines()
         ref_lines = ref.get_lines()
 
         if user_lines is None:
-            return bad(f"No lines were plotted in '{name}'")
+            return bad("No lines were plotted in '%s'" % name)
 
         if len(user_lines) != len(ref_lines):
-            return bad(f"{len(user_lines)} lines were plotted in '{name}' but {len(ref_lines)} lines were "
+            return bad("%d lines were plotted in '%s' but %d lines were "
                        "expected"
-                      )
+                       % (len(user_lines), name, len(ref_lines)))
 
         ref_datas = {}
         for i, ref_line in enumerate(ref_lines):
@@ -485,10 +519,10 @@ class Feedback:
 
         if num_correct == len(ref_lines):
             if report_success:
-                cls.add_feedback(f"'{name}' looks good")
+                cls.add_feedback("'%s' looks good" % name)
             return True
         else:
-            return bad(f"'{name}' is inaccurate")
+            return bad("'%s' is inaccurate" % name)
 
 
     @classmethod
@@ -500,8 +534,11 @@ class Feedback:
         Checks and adds feedback regarding the correctness of
         a pandas ``DataFrame``.
         Author: Wade Fagen-Ulmschneider (waf)
+
         By default, checks if the student DataFrame ``data`` contains the same contents as the reference DataFrame ``ref`` by using ``pandas.util.testing.assert_frame_equal`` after basic sanity checks.
+
         Parameters:
+
         - ``name``, String: The human-readable name of the DataFrame being checked
         - ``ref``, DataFrame: The reference (correct) DataFrame
         - ``data``, DataFrame: The student DataFrame
